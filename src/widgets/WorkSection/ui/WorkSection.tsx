6193c1ff8efdaa394f3f30d25bb283e023369b60
avs-ht <151386266+avs-ht@users.noreply.github.com>
2024-05-24 19:57:39 +0300

    Work Section Adaptive

--- src/widgets/WorkSection/ui/WorkSection.tsx
+++ src/widgets/WorkSection/ui/WorkSection.tsx
@@ -1,15 +1,12 @@
 import styles from "./WorkSection.module.scss";
 import WorkForm from "@/features/WorkForm";
 import { Container } from "@/shared/ui/Container";
-import WorkerImage from "./worker.jpg";
-import Image from "next/image";
+
 export const WorkSection = () => {
   return (
     <section className={styles.section}>
       <Container className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image src={WorkerImage} alt="Работник кофейни Бодрый день" />
-        </div>
+        <div className={styles.imageContainer}></div>
         <WorkForm />
       </Container>
     </section>
